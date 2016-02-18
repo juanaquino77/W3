99df419e739a32929e632d410d677ff1d1478bbd
juanaquino77 <juanaquino77@gmail.com>
2016-02-18 18:34:48 -0300

    commit final
    Terminados los arreglos de diseño para IE8.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("#jquery_jplayer_1").jPlayer({
+		ready: function () {
+			$(this).jPlayer("setMedia", {
+				title: "Big Buck Bunny",
+				m4v: "http://www.jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v",
+				ogv: "http://www.jplayer.org/video/ogv/Big_Buck_Bunny_Trailer.ogv",
+				webmv: "http://www.jplayer.org/video/webm/Big_Buck_Bunny_Trailer.webm",
+				poster: "http://www.jplayer.org/video/poster/Big_Buck_Bunny_Trailer_480x270.png"
+			});
+		},
+		swfPath: "video/jPlayer-2.9.2/dist/jplayer",
+		supplied: "webmv, ogv, m4v",
+		size: {
+			width: "100%",
+			height: "100%",
+			cssClass: "jp-video-360p"
+		},
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true,
+		remainingDuration: true,
+		toggleDuration: true
+	});
+});
